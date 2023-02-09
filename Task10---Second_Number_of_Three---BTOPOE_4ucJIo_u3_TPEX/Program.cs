d36593e9888d7caf81fbc33b9b5b8aadc13258c3
viktor <shurganovvv@yandex.ru>
2023-02-10 00:28:04 +0300

    end TasksAtHome02 project

---
+++ Task10---Second_Number_of_Three---BTOPOE_4ucJIo_u3_TPEX/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 10: Напишите программу, которая 
+//     принимает на вход трёхзначное число 
+//     и на выходе показывает вторую цифру этого числа.
+//         456 -> 5
+//         782 -> 8 
+//         918 -> 1
+
+Console.WriteLine("Введите трёхзначное число: ");
+int i;
+for (i = 0; i < 3; i++)
+{
+    int number = Convert.ToInt32(Console.ReadLine());
+    // как вариант: int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+    if (number > 99 && number < 1000)
+    {
+        // переменная по необходимости: int lastDigit = number / 10 % 10;
+        System.Console.WriteLine(number / 10 % 10);
+        i = 5;
+    }
+    else if (number < -99 && number > -1000)
+    {
+        // переменная по необходимости: int lastDigit = (number / 10 % 10) * -1;
+        System.Console.WriteLine(-1 * number / 10 % 10);
+        i = 5;
+    }
+    else
+    {
+        System.Console.WriteLine("Пожалуйста, ТРЁХзначное число!");
+    }
+}
+if (i < 5)
+{
+    System.Console.WriteLine();
+    System.Console.WriteLine("Пнятненько...");
+    System.Console.WriteLine(" Ошибочка вышла");
+    System.Console.WriteLine("Извини...");
+    System.Console.WriteLine();
+}
+
+/* для интереса усложнил себе задачу, 
+понял, что поторопился, когда увидил условия второй задачи... 
+наверно, надо постараться себя сдерживать? */
---
+++ Task13---Third_digit_of_number/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 13: Напишите программу, которая 
+//     выводит третью цифру заданного числа 
+//     или сообщает, что третьей цифры нет.
+//         645 -> 5
+//         78 -> третьей цифры нет
+//         32679 -> 6
+
+Console.WriteLine("Какими часлами побалуешь на этот раз?");
+Console.WriteLine("Только давай сразу договоримся: пусть число будет не очень большим, ну, то есть в пределах -2 147 483 648 и + 2 147 483 647, хорошо?");
+Console.Write("Означь: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == -2147483648) { 
+    // Console.WriteLine("4, соригинальничал, да?"); 
+    number = 214;
+    }
+else if (number < 0) { number = number * -1; }
+if (number < 100) { Console.WriteLine("третьей цифры нет"); }
+else
+{
+    while (number / 1000 > 0) { number = number / 10; }
+    Console.WriteLine(number % 10);
+}
+
+/* а вот интересно, 
+как нужно конвертировать Console.ReadLine()
+для получения вещественного типа данных?
+    int number = Convert.ToInt32(Console.ReadLine());
+    double numbers = Convert.Double64(Console.ReadLine( :)));
+    ? */
